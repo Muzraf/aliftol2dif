f77e7733d8220ba5f76b557c40940f4bbd53f762
Mohammed Musharaf <mohamedmusharaf687@gmail.com>
2025-05-08 22:41:02 +0530

    initial commit

---
+++ aliftol2dif.sh
@@ -0,0 +1,7 @@
+magick $1 -crop 100%x16.667% +repage o%d.png
+magick o0.png o1.png o2.png o3.png o4.png o5.png +append $2.1.png
+
+magick $2.1.png -gravity North -background white -splice 0x1 -background black -splice 0x1 -trim +repage -chop 0x1 $2.2.png
+magick $2.2.png -gravity South -background white -splice 0x1 -background black -splice 0x1 -trim +repage -chop 0x1 $2
+
+rm o0.png o1.png o2.png o3.png o4.png o5.png o6.png $2.1.png $2.2.png
