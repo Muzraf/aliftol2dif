fd874b2bd3b8c00f0935e7ab4050cef11bcf2cd4
Mohammed Musharaf <mohamedmusharaf687@gmail.com>
2025-05-09 17:14:51 +0530

    added a bash var

--- aliftol2dif.sh
+++ aliftol2dif.sh
@@ -1,7 +1,11 @@
+
+
+A="aliftol2dif__m__"
+
 echo "Breaking image and"
-magick $1 -crop 100%x16.667% +repage o%d.png
+magick $1 -crop 100%x16.667% +repage ${A}%d.png
 echo "Sequencing it..."
-magick o0.png o1.png o2.png o3.png o4.png o5.png +append $2.1.png
+magick ${A}0.png ${A}1.png ${A}2.png ${A}3.png ${A}4.png ${A}5.png +append ${2}.1.png
 
 echo "trimming bottom border"
 magick $2.1.png -gravity North -background white -splice 0x1 -background black -splice 0x1 -trim +repage -chop 0x1 $2.2.png
@@ -9,4 +13,4 @@ echo "trimming top border"
 magick $2.2.png -gravity South -background white -splice 0x1 -background black -splice 0x1 -trim +repage -chop 0x1 $2
 
 echo "removing tmp files..."
-rm o0.png o1.png o2.png o3.png o4.png o5.png o6.png $2.1.png $2.2.png
+rm ${A}0.png ${A}1.png ${A}2.png ${A}3.png ${A}4.png ${A}5.png ${A}6.png $2.1.png $2.2.png
